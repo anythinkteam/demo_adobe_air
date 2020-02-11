c55dea8a3d6a4fa0dcb4239c64bee2f289b67240
zhangshiren <285995476@qq.com>
2020-02-11 20:43:40 +0800

    1.移除部分无用的ane包 2.更新Android的ANE插件包 3.更新穿山甲的SDK包

--- build_ane.py
+++ build_ane.py
@@ -7,9 +7,10 @@ import os
 UPARPU_VERSION = "3.5.0"
 RELEASE_DIRECTORY = "ane_release"
 # target list
-TARGETS_LIST = ['ane_build', 'adapter_adcolony', 'adapter_admob', 'adapter_applovin', 'adapter_baidu', 'adapter_chartboost'                # Native Adapters
-                , 'adapter_facebook', 'adapter_flurry', 'adapter_gdt', 'adapter_inmobi', 'adapter_ironsource', 'adapter_maio', 'adapter_mintegral', 'adapter_mobpower', 'adapter_nend', 'adapter_oneway', 'adapter_tapjoy', 'adapter_toutiao'                # Rewarded Video Adapters
-                , 'adapter_uniplay', 'adapter_unityads', 'adapter_vungle']
+# TARGETS_LIST = ['ane_build', 'adapter_adcolony', 'adapter_admob', 'adapter_applovin', 'adapter_baidu', 'adapter_chartboost'                # Native Adapters
+#                 , 'adapter_facebook', 'adapter_flurry', 'adapter_gdt', 'adapter_inmobi', 'adapter_ironsource', 'adapter_maio', 'adapter_mintegral', 'adapter_mobpower', 'adapter_nend', 'adapter_oneway', 'adapter_tapjoy', 'adapter_toutiao'                # Rewarded Video Adapters
+#                 , 'adapter_uniplay', 'adapter_unityads', 'adapter_vungle']
+TARGETS_LIST = ['ane_build','adapter_admob','adapter_facebook','adapter_mintegral','adapter_nend', 'adapter_maio', 'adapter_toutiao', 'adapter_unityads']
 
 
 def build_ane():
