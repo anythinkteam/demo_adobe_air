b63fea8fbc47892705bf619978300c4cb92c70fc
lihongyan <lihongyan@sa.com>
2020-02-11 14:15:07 +0800

    update nend ios sdk

---
+++ adapter_nend/iOS_project/AneNendAdapter/ATAneNendAdapter/ATAneNendAdapter.h
@@ -0,0 +1,13 @@
+//
+//  ATAneNendAdapter.h
+//  ATAneNendAdapter
+//
+//  Created by stephen on 2019/4/23.
+//  Copyright © 2019 uparpu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ATAneNendAdapter : NSObject
+
+@end
--- adapter_nend/ios/NendAd.framework/Headers/NADNativeVideoView.h
+++ adapter_nend/ios/NendAd.framework/Headers/NADNativeVideoView.h
@@ -21,6 +21,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)nadNativeVideoViewDidFailToPlay:(NADNativeVideoView *)videoView;
 - (void)nadNativeVideoViewDidOpenFullScreen:(NADNativeVideoView *)videoView;
 - (void)nadNativeVideoViewDidCloseFullScreen:(NADNativeVideoView *)videoView;
+- (void)nadNativeVideoViewDidStartFullScreenPlaying:(NADNativeVideoView *)videoView;
+- (void)nadNativeVideoViewDidStopFullScreenPlaying:(NADNativeVideoView *)videoView;
 
 @end
 
@@ -28,7 +30,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (readwrite, nonatomic, weak) id<NADNativeVideoViewDelegate> delegate;
 @property (readwrite, nonatomic, strong) NADNativeVideo *videoAd;
-@property (readwrite, nonatomic, weak, nullable) UIViewController *rootViewController;
+@property (readwrite, nonatomic, weak, nullable) IBOutlet UIViewController *rootViewController;
+
+- (instancetype)init __deprecated_msg("This method is deprecated. Use initWithRootViewController: method instead.");
+- (instancetype)initWithFrame:(CGRect)frame __deprecated_msg("This method is deprecated. Use initWithFrame:rootViewController: method instead.");
+
+- (instancetype)initWithRootViewController:(UIViewController *)rootViewController;
+- (instancetype)initWithFrame:(CGRect)frame rootViewController:(UIViewController *)rootViewController;
 
 @end
 NS_ASSUME_NONNULL_END
