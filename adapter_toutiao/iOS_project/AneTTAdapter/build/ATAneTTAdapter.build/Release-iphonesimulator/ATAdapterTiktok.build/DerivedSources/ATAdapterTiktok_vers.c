4a99448ebf17efe17ad850a2e5ff0739b647343c
lihongyan <lihongyan@sa.com>
2020-02-11 20:07:38 +0800

    delete some build files

--- adapter_toutiao/iOS_project/AneTTAdapter/build/ATAneTTAdapter.build/Release-iphonesimulator/ATAdapterTiktok.build/DerivedSources/ATAdapterTiktok_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char ATAdapterTiktokVersionString[];
- extern const double ATAdapterTiktokVersionNumber;
-
- const unsigned char ATAdapterTiktokVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ATAdapterTiktok  PROJECT:ATAneTTAdapter-1" "\n";
- const double ATAdapterTiktokVersionNumber __attribute__ ((used)) = (double)1.;
--- adapter_toutiao/iOS_project/AneTTAdapter/build/ATAneTTAdapter.build/Release-iphonesimulator/ATAdapterTiktok.build/Objects-normal/x86_64/ATAdapterTiktok_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/stephen/Desktop/work/SuperAds/git/UpPlatform/uparpu_github/demo_adobe_air/adapter_toutiao/iOS_project/AneTTAdapter/build/ATAneTTAdapter.build/Release-iphonesimulator/ATAdapterTiktok.build/DerivedSources/ATAdapterTiktok_vers.c
