25b06c75de23d9ea26353505760d2a1584c3d479
One2ThreeZJM <253521624@qq.com>
2020-06-08 15:51:31 +0800

    去掉sdk

--- adapter_chartboost/iOS_project/UpArpuAneChartboostAdapter/build/UpArpuAneChartboostAdapter.build/Release-iphonesimulator/UpArpuAdapterChartboost.build/Objects-normal/x86_64/UpArpuAdapterChartboost_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/stephen/Desktop/work/SuperAds/git/UpPlatform/up_sdk_ane/adapter_chartboost/iOS_project/UpArpuAneChartboostAdapter/build/UpArpuAneChartboostAdapter.build/Release-iphonesimulator/UpArpuAdapterChartboost.build/DerivedSources/UpArpuAdapterChartboost_vers.c
--- adapter_tapjoy/iOS_project/UpArpuAneTapjoyAdapter/build/UpArpuAneTapjoyAdapter.build/Release-iphonesimulator/UpArpuAdapterTapjoy.build/Objects-normal/x86_64/UpArpuAdapterTapjoy_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/stephen/Desktop/work/SuperAds/git/UpPlatform/up_sdk_ane/adapter_tapjoy/iOS_project/UpArpuAneTapjoyAdapter/build/UpArpuAneTapjoyAdapter.build/Release-iphonesimulator/UpArpuAdapterTapjoy.build/DerivedSources/UpArpuAdapterTapjoy_vers.c
